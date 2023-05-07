93775a5f996e2d764b71a0ee92830beec22978c9
issdn <isdnblk@gmail.com>
2023-05-07 21:10:57 +0200

    v0.10001

---
+++ src/routes/+error.svelte
@@ -0,0 +1,7 @@
+<div
+	class="w-full h-full bg-secondary-gradient text-white text-xl gap-y-4 md:text-5xl flex flex-col font-lilita items-center justify-center"
+>
+	<h1>404</h1>
+	<p>Page not found</p>
+	<a href="/">🔙</a>
+</div>
--- src/routes/events/+server.ts
+++ src/routes/events/+server.ts
@@ -1,9 +1,5 @@
 import { BRAWL_API_KEY } from '$env/static/private';
 
-export const config = {
-	runtime: 'edge'
-};
-
 export const GET = async () => {
 	const response = await fetch(`https://api.brawlstars.com/v1/events/rotation`, {
 		headers: {
